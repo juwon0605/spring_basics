0e07b2310b7bbd5c63408074fd83acb30a25180c
juwon <58173061+juwon0605@users.noreply.github.com>
2021-07-01 00:33:02 +0900

    spring boot setting

---
+++ src/main/java/spring/spring_basics/SpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package spring.spring_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/spring_basics/SpringBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.spring_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
