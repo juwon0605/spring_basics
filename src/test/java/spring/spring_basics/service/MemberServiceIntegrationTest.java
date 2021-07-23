ecdeaff0708e3331edaec8bdfab854ea0645a15b
juwon <58173061+juwon0605@users.noreply.github.com>
2021-07-23 23:52:50 +0900

    스프링 통합 테스트

--- src/test/java/spring/spring_basics/service/MemberServiceIntegrationTest.java
+++ src/test/java/spring/spring_basics/service/MemberServiceIntegrationTest.java
@@ -1,43 +1,49 @@
 package spring.spring_basics.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import spring.spring_basics.domain.Member;
 import spring.spring_basics.repository.MemberRepository;
 import spring.spring_basics.repository.MemoryMemberRepository;
 
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-// 장점: 스프링 컨테이너와 함께 테스트하는 것보다 훨씬 빠르다(단위 테스트)
-class MemberServiceTest {
-
-//    new()를 통해 사실상 다른 테스트 코드와 매번 다른 리소스 DB(Memory)를 사용하는 것이니 아래와 같이 작업
-//    모든 컨트롤러 클래스에서 동일한 리소스 한 개만 사용하도록 아래와 같이 작업(스프링 컨테이너 스프링 빈 싱글톤 등록)
+@SpringBootTest // 단순 java코드로 작동만 검증하는게 아니라 스프링 컨테이너 실행까지 테스트
+@Transactional  // 테스트코드에 사용하면 테스트코드 함수마다 db clear을 위한 작업 수행(테스트마다 시작 전으로 롤백)
+class MemberServiceIntegrationTest {
+    
 /*
     MemberService memberService = new MemberService();
     MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 */
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
+//    매번 리소스 새로 할당
+/*
+@BeforeEach
+public void beforeEach(){
+    memberRepository = new MemoryMemberRepository();
+    memberService = new MemberService(memberRepository);
+}
+*/
+    
+//    new()를 통해 사실상 다른 테스트 코드와 매번 다른 리소스 DB(Memory)를 사용하는 것이니 아래와 같이 작업
+//    모든 컨트롤러 클래스에서 동일한 리소스 한 개만 사용하도록 아래와 같이 작업(스프링 컨테이너 스프링 빈 싱글톤 등록)
+    // 테스트 리소스는 테스트 하고 끝나니 편한 방법으로 작성
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
+//    @Transactional이 아래 작업을 자동으로 수행
+/*
 //    테스트코드 함수마다 객체 clear을 위한 작업
     @AfterEach
     public void afterEach(){
         memberRepository.clearStore();
     }
-
-
+*/
 
     @Test
 //    테스트 코드는 실제 빌드되는 코드에 반영되지 않아서 다국적으로 일하는게 아니면 한글로 작성할 수도 있음
