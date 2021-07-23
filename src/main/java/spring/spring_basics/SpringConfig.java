8c7925f5cea36421c3deae60377b781afdb0a94d
juwon <58173061+juwon0605@users.noreply.github.com>
2021-07-23 23:23:00 +0900

    JdbcMemberRepository.java setting
    MemberRepository implement Jdbc 구현
    MemoryMemberRepository 대체

--- src/main/java/spring/spring_basics/SpringConfig.java
+++ src/main/java/spring/spring_basics/SpringConfig.java
@@ -1,10 +1,15 @@
 package spring.spring_basics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.spring_basics.repository.JdbcMemberRepository;
 import spring.spring_basics.repository.MemberRepository;
 import spring.spring_basics.repository.MemoryMemberRepository;
 import spring.spring_basics.service.MemberService;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 // 수동으로 스프링 컨테이너에 스프링 빈을 등록하는 방법
 // 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용한다.
@@ -14,14 +19,24 @@ import spring.spring_basics.service.MemberService;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//    개방-폐쇄 원칙(OCP, Open-Closed Principle)
+//    기능 확장에는 열려있고, 수정.변경에는 닫혀있다.
+//    스프링의 DI(Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현클래스를 변경할 수 있다.
+    public MemberRepository memberRepository(){ // MemberRepository는 implement로 구현하여 아래와 같이 다형성 활용
+//        return new MemoryMemberRepository();  // implement 구현체를 MemoryMemberRepository로 구현
+        return new JdbcMemberRepository(dataSource); // implement 구현체를 JdbcMemberRepository로 구현
     }
 
 }
